80fb2e5c4224cd8f1861ba071f5a376627f167a1
Neithiri <leonie.deu@outlook.fr>
2022-05-17 21:20:41 +0200

    Update mass_flow_rate.m

--- mass_flow_rate.m
+++ mass_flow_rate.m
@@ -1,18 +1,16 @@
-function [m_dot] = mass_flow_rate(P_gt, P_c, R_feed, R_dyn, R_inj, R_valves)
+function [m_dot] = mass_flow_rate(P_gt, P_c, R_tot)
 %MASS_FLOW_RATE This function returns the mass flow rate taking into
-%account the losses through the feeding system, the pressure of the tank
-%and the combustion chanmber pressure.
+%account the losses through the feeding system, the pressure of the gas
+%inside the tank and the combustion chamber pressure.
 %
 % PROTOTYPE:
-%   [m_dot] = mass_flow_rate(P_gt, P_c, R_feed, R_dyn, R_inj, R_valve)
+%   [m_dot] = mass_flow_rate(P_gt, P_c, R_tot)
 %
 % INPUT:
-%  P_gt         Pressure
+%  P_gt         Gas pressure in tank [Pa]
 %  P_c          Combustion chamber pressure [Pa]
-%  R_feed       Pressure drop coefficient due to feeding line [Pa/(kg^2/s^2)]
-%  R_dyn        Pressure drop coefficient due to dynamic losses [Pa/(kg^2/s^2)]
-%  R_inj        Pressure drop coefficient due to injection plate [Pa/(kg^2/s^2)]
-%  R_valves     Pressure drop coefficient due to valve [Pa/(kg^2/s^2)]
+%  R_tot        Sum of all resistance coefficients for propellant flow path
+%               [Pa/(kg^2/s^2)]
 %
 % OUTPUT:
 %  m_dot        Mass flow rate [kg/s]
@@ -23,8 +21,7 @@ function [m_dot] = mass_flow_rate(P_gt, P_c, R_feed, R_dyn, R_inj, R_valves)
 % VERSIONS
 %   2022-05-17:     First version
 
-R_sum = R_feed + R_dyn + R_inj + R_valves; % [Pa/(kg^2/s^2)]
-m_dot = sqrt( (P_gt-P_c)/ R_sum); % [kg/s]
+m_dot = sqrt( (P_gt-P_c)/R_tot ); % [kg/s]
 
 end
 
