f75df8c2df1e77ca9e796afbf32eb3a35c2608a9
Neithiri <leonie.deu@outlook.fr>
2022-05-17 16:35:32 +0200

    Update feeding_losses.m

--- feeding_losses.m
+++ feeding_losses.m
@@ -1,19 +1,19 @@
-function [deltaP_feed] = feeding_losses(f, rho, L, D, m_dot)
-%FEEDING_LOSSES This function returns the pressure drop due to feeding
-%line.
+function [R_feed] = feeding_losses(f, rho, L, D)
+%FEEDING_LOSSES This function returns the coefficient that multiplies
+% the square of the mass flow rate in the expression of the pressure drop
+% due to feeding line.
 %
 % PROTOTYPE:
-%   [deltaP_feed] = feeding_losses(f, rho, L, D, m_dot)
+%   [R_feed] = feeding_losses(f, rho, L, D)
 %
 % INPUT:
 %  f            Darcy friction factor [-]
 %  rho          Propellant density [kg/m^3]
 %  L            Length of the feeding line [m]
 %  D            Diameter of the pipe in the feeding line [m]
-%  m_dot        Propellant mass flow rate [kg/m^3]
 %
 % OUTPUT:
-%  deltaP_feed  Pression drop due to feeding line [Pa]
+%  R_feed       Pression drop coefficient [Pa/(kg^2/s^2)]
 %
 % CONTRIBUTORS:
 %   Léonie DEU
@@ -21,7 +21,7 @@ function [deltaP_feed] = feeding_losses(f, rho, L, D, m_dot)
 % VERSIONS
 %   2022-05-17:     First version
 
-deltaP_feed = 8/pi^2*f*m_dot^2/rho*L/D^5; % [Pa] Pressure drop
+R_feed = 8/pi^2*f/rho*L/D^5; % [Pa/(kg^2/s^2)]
 
 end
 
