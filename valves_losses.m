131eab1edcf0ae2f516aa1dd4ccc43eaec0715f3
Neithiri <leonie.deu@outlook.fr>
2022-05-17 18:02:28 +0200

    Update valves_losses.m

--- valves_losses.m
+++ valves_losses.m
@@ -4,10 +4,10 @@ function [R_valves] = valves_losses(rho)
 rho_H2O = 1000; % [kg/m^3]
 
 C_v_open = 13; % flow coefficient for open-close valve
-C_v_check = 0; % flow coefficient for check valve
+C_v_check = 1.5; % flow coefficient for check valve
 
 K_v_open = 0.865*C_v_open; % flow factor for open-close valve [m^3/h]
-K_v_check = 0.865*C_v_check % flow factor for check valve [m^3/h]
+K_v_check = 0.865*C_v_check; % flow factor for check valve [m^3/h]
 
 R_open = (3600^2/rho)/(rho_H2O*K_v_open^2);
 R_check = (3600^2/rho)/(rho_H2O*K_v_check^2);
