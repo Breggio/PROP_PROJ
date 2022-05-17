b5089367dde3d7cfac46b6489e1eabeb90a6ff3e
estersommariva <estersommariva1999@gmail.com>
2022-05-17 17:45:51 +0200

    cea

--- CEAM 1.0.0.0/prova1.m
+++
@@ -1,8 +0,0 @@
-
-p_cc = 0.5:0.5:30;
-
-[T,m_mol,c_p,Isp] = CEA_computation(p_cc,nan);
-
-plot(Isp)
-
-
---
+++ prova1.m
@@ -0,0 +1,8 @@
+
+p_cc = 0.5:0.5:30;
+
+[T,m_mol,c_p,Isp] = CEA_computation(p_cc,nan);
+
+plot(Isp)
+
+
