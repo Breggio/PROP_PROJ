491f7b63279499a35ff19611c1f4e2a24d979d5d
Neithiri <leonie.deu@outlook.fr>
2022-05-17 16:35:36 +0200

    Update injection_losses.m

--- injection_losses.m
+++ injection_losses.m
@@ -1,18 +1,18 @@
-function [deltaP_inj] = injection_losses(rho, m_dot, A_inj, C_d)
-%INJECTION_LOSSES This function returns the pressure drop due to injection
-%plates.
+function [R_inj] = injection_losses(rho, A_inj, C_d)
+%INJECTION_LOSSES This function returns the coefficient that multiplies
+% the square of the mass flow rate in the expression of the pressure drop
+% due to injection plate.
 
 % PROTOTYPE:
-%   [deltaP_inj] = injection_losses(rho, m_dot, A_inj, C_d)
+%   [R_inj] = injection_losses(rho, A_inj, C_d)
 %
 % INPUT:
 %  rho          Propellant density [kg/m^3]
-%  m_dot        Propellant mass flow rate [kg/m^3]
 %  A_inj        Area of one propellant injector [m^2]
 %  Cd           Discharge coefficient [-]
 %
 % OUTPUT:
-%  deltaP_inj  Pression drop due to injection plates [Pa]
+%  R_inj        Pression drop coefficient [Pa/(kg^2/s^2)]
 %
 % CONTRIBUTORS:
 %   Léonie DEU
@@ -20,7 +20,7 @@ function [deltaP_inj] = injection_losses(rho, m_dot, A_inj, C_d)
 % VERSIONS
 %   2022-05-17:     First version
 
-deltaP_inj = 8/rho*(m_dot/(A_inj*C_d))^2;
+R_inj = (8/rho)/(A_inj*C_d)^2;
 
 end
 
