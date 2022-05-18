5ff33d7a6ba1a9e54295a4fb1c3992ee1118fcf5
Neithiri <leonie.deu@outlook.fr>
2022-05-18 11:15:19 +0200

    Update valves_losses.m

--- valves_losses.m
+++ valves_losses.m
@@ -20,17 +20,34 @@ function [R_valves] = valves_losses(rho)
 %   2022-05-17:     First version
 
 rho_H2O = 1000; % water density [kg/m^3]
+SG = rho/rho_H2O; % [-] specific gravity
 
-C_v_open_close = 13; % flow coefficient for open-close valve
-C_v_check = 1.5; % flow coefficient for check valve
+%% Computation of C_v for each valve
+% Latch valve (open-close valve) p.9 on datasheet
+deltaP_latch = 19; % [psi] Pressure drop
+m_dot_latch = 0.4/0.14; % volumetric mass flow rate [gallons/min]
+% (lb/s -> gallons/min)
 
-K_v_open_close = 0.865*C_v_open_close; % flow factor for open-close valve [m^3/h]
-K_v_check = 0.865*C_v_check; % flow factor for check valve [m^3/h]
+C_v_latch = m_dot_latch*sqrt(SG/deltaP_latch);
 
-R_open_close = (3600^2/rho)*1/(rho_H2O*K_v_open_close^2);
+% Check valve
+deltaP_check = 3.0; % [psi] Pressure drop
+m_dot_check = 3.0/0.14; % [gallons/min] Volumetric mass flow rate
+
+C_v_check = m_dot_check*sqrt(SG/deltaP_check);
+
+%% Computation of K_v for each valve
+K_v_latch = 0.865*C_v_latch; % [m^3/h] Flow factor
+K_v_check = 0.865*C_v_check; % [m^3/h] Flow factor
+
+% C_v_open_close = 13; % flow coefficient for open-close valve
+% C_v_check = 1.5; % flow coefficient for check valve
+
+%% Computation of resistance coefficients
+R_latch = (3600^2/rho)*1/(rho_H2O*K_v_latch^2);
 R_check = (3600^2/rho)*1/(rho_H2O*K_v_check^2);
 
-R_valves = R_open_close + R_check;
+R_valves = R_latch + R_check;
 
 end
 
