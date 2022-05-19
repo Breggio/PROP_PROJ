792b5a9e2e91c145da69990fe8749dd5cb08ae17
BouleuxA <alexandreclement.bouleux@mail.polimi.it>
2022-05-19 12:32:35 +0200

    Create control_valve_pressure_loss.m

---
+++ control_valve_pressure_loss.m
@@ -0,0 +1,6 @@
+function pressure_loss = control_valve_pressure_loss(given_pressure_loss, given_flow_rate, give_volumic_mass, real_flow_rate, real_volumic_mass)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
