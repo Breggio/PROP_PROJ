5d25370260a85fc7a25a73c748f9949e2396beaa
BouleuxA <alexandreclement.bouleux@mail.polimi.it>
2022-05-19 14:10:31 +0200

    Update control_valve_pressure_loss.m

--- control_valve_pressure_loss.m
+++ control_valve_pressure_loss.m
@@ -1,6 +1,21 @@
 function pressure_loss = control_valve_pressure_loss(given_pressure_loss, given_flow_rate, give_volumic_mass, real_flow_rate, real_volumic_mass)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%Returns the pressure loss of a control valve at our conditions, given the
+%loss given in the datasheet of the valve.
+%   CAREFUL: GIVE NUMBERS IN THE DEMANDED UNITS (else results WILL be wrong)
+%   INPUTS : 
+%   given_pressure_loss : the pressure drop given in the datasheet
+%   (bar, or else problems)
+%   given_flow_rate : the volumetric flow rate for the given pressure drop
+%   in the datasheet (m³/s)
+%   given_volumic_mass : the volumic mass of the fluid used in the
+%   datasheet (usually water, but check) (kg/m³)
+%   real_flow_rate : the volumetric flow rate at the valve in the designed system (m³/s) 
+%   real_volumic_mass : the volumic mass of the fluid used in the real
+%   system (kg/m³)
+%   OUTPUT : pressure_loss : the pressure drop induced by the valve (bar)
+%   VERSIONS : 2022/05/19, Alexandre BOULEUX
+%
+
+pressure_loss = given_pressure_loss * (real_flow_rate/given_flow_rate)^2 * (real_volumic_mass/give_volumic_mass);
+
 end
