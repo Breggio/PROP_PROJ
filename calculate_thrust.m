3005e0cbd2441b4378e03973f62b343513feb4c1
estersommariva <estersommariva1999@gmail.com>
2022-05-20 14:34:43 +0200

    MC22222222

--- calculate_thrust.m
+++ calculate_thrust.m
@@ -1,10 +1,10 @@
 function T = calculate_thrust(d_inj_ox, d_inj_f, A_th, Pc, Cd, rho_ox, rho_f)
 
-Delta_P_inj= 0.2*Pc;
+Delta_P_inj= 0.25*Pc;
 
 A_inj_ox = (pi*d_inj_ox^2)/4;
 A_inj_f = (pi*d_inj_f^2)/4;
-m_dot_ox = A_inj_ox*Cd*sqrt(2*Delta_P_inj*rho_ox);
+m_dot_ox = 2*A_inj_ox*Cd*sqrt(2*Delta_P_inj*rho_ox);
 m_dot_f = A_inj_f*Cd*sqrt(2*Delta_P_inj*rho_f);
 
 OF = m_dot_ox/m_dot_f;
